efb10e41d04eacdcebb1746224159404df15fddd
BartugKaan <bartugkaan.celebi@outlook.com>
2023-02-13 16:29:09 +0300

    Added : a function for ending editing with keyboard

--- AlisVerisListsi/DetailsViewController.swift
+++ AlisVerisListsi/DetailsViewController.swift
@@ -8,18 +8,24 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
-
     @IBOutlet weak var isimTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var fiyatTextField: UITextField!
     @IBOutlet weak var urunTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(klavyeyiKapat))
+        view.addGestureRecognizer(gestureRecognizer)
     }
     
     @IBAction func kaydetButtonTiklandi(_ sender: Any) {
+    }
+    
+    @objc func klavyeyiKapat(){
+        view.endEditing(true)
+        
     }
     
 }
