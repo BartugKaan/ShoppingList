0842cb891aca2fc9badf0a42864b95f49c0c5dc1
BartugKaan <bartugkaan.celebi@outlook.com>
2023-02-13 16:12:33 +0300

    Added : for UI crated new viewController

---
+++ AlisVerisListsi/DetailsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailsViewController.swift
+//  AlisVerisListsi
+//
+//  Created by Bartuğ Kaan Çelebi on 13.02.2023.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var isimTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var fiyatTextField: UITextField!
+    @IBOutlet weak var urunTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func kaydetButtonTiklandi(_ sender: Any) {
+    }
+    
+}
