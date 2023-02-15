b8daca59934998c4f48b966f4272277e1426d73c
BartugKaan <bartugkaan.celebi@outlook.com>
2023-02-15 16:38:26 +0300

    Added : User can select a photo from library for image of product

--- AlisVerisListsi/ViewController.swift
+++ AlisVerisListsi/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
         
     }
 
-    @objc func eklemeButonuTiklandi(){
+    @objc private func eklemeButonuTiklandi(){
         performSegue(withIdentifier: "toDetailVC", sender: nil)
         
     }
